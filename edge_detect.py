5eeb68fe7bb7b594c320a5a1257efeaea085d9a7
DizzyYunxuan <516488199@qq.com>
2021-11-14 23:52:51 +0800

    update decision map

--- edge_detect.py
+++ edge_detect.py
@@ -6,52 +6,43 @@ from scipy import ndimage
 
 img = r"D:\anaconda\flappy_ai\FlapPyBird\datasets\1636268284.jpg"
 img = cv2.imread(img)
-img = np.mean(img, axis=2)
-img = img[30:436, :]
-
-# gradient
-# img_pad = np.pad(img, pad_width=((0, 0), (1, 0)), mode='edge')
-# img_g_h = img_pad[:,:-1] - img
-# img_g_h[img_g_h<0] = 0
-# img_g_h[img_g_h>0] = 1
-
-# img_pad = np.pad(img, pad_width=((1, 0), (0, 0)), mode='edge')
-# img_g_v = img_pad[:-1,:] - img
-# img_g_v[img_g_v<0] = 0
-# img_g_v[img_g_v>0] = 1
-
-# img_edge = img_g_h + img_g_v
-# print()
-
-# wavelet
-# print()
-# k = np.array([1, 1,-1, -1])
-# img_blur_v = cv2.filter2D(src=img, ddepth=-1, kernel=k)
-# img_blur_h = cv2.filter2D(src=img.T, ddepth=-1, kernel=k).T
-# print()
-# edges = img - img_blur
+img = np.mean(img, axis=2) / 255.
+img = img[34:434, 4:-4]
+[h, w] = img.shape
 
 
 # lap egdes
-# k = np.array([[-1, -1, -1],
-#                 [-1, 8, -1],
-#                 [-1, -1, -1]])
-k = np.array([[0, -1, 0],
-                [-1, 4, -1],
-                [0, -1, 0]])
-img_edge = cv2.filter2D(img, -1, k)
-img_edge[img_edge<20] = 0
-img_edge[img_edge>=20] = 255
+k = np.array([[-1, -1, -1],
+                [-1, 8, -1],
+                [-1, -1, -1]])
 
-nb_obj, img_label = cv2.connectedComponents(np.uint8(img_edge), connectivity=4)
+s_k = np.ones([3, 3]) / 9
+img_edge = cv2.filter2D(cv2.filter2D(img, -1, k), -1, s_k)
+img_seg = img_edge != 0.0
+img_seg = img_seg.astype(np.float64)
+
+
+decision_map = np.zeros_like(img_edge)
+
+
+h_strip = img_seg[16, :]
+h_strip_shuffle  = np.pad(h_strip, pad_width=((0, 1)), mode='constant')
+h_sePoints = h_strip_shuffle[1:] - h_strip
+h_sePoints = np.argwhere(h_sePoints != 0).flatten()
+h_start_points = h_sePoints[::2]
+h_start_points -= 2
+h_end_points = h_sePoints[1::2]
+h_end_points += 2
+if len(h_end_points) - len(h_start_points) == 1:
+    end_points = h_end_points + [len(h_strip) - 1]
+h_ob_lr_tuple = np.array(list(zip(h_start_points, h_end_points)))
+
+v_strip = img_seg[:, np.int((h_ob_lr_tuple[:, 0] + h_ob_lr_tuple[:, 1]) / 2)]
+v_strip_shuffle = np.pad(v_strip, pad_width=((1, 0)), mode='constant')
+v_sePoints = v_strip_shuffle[:-1] - v_strip
+v_sepoints = np.argwhere(v_sePoints != 0)
 
 print()
-
-# OSTU
-# hist = np.histogram(img, bins=255)
-# print(hist)
-
-# print(edges.min(), edges.max())
 
 
 
