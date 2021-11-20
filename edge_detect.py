0d57aefb0f20f3af4b9fc4e67193e0cb7bf2d29f
DizzyYunxuan <516488199@qq.com>
2021-11-21 01:54:52 +0800

    detect obstructs

--- edge_detect.py
+++ edge_detect.py
@@ -4,10 +4,10 @@ from matplotlib import pyplot as plt
 from PIL import Image
 from scipy import ndimage
 
-img = r"D:\anaconda\flappy_ai\FlapPyBird\datasets\1636268284.jpg"
-img = cv2.imread(img)
-img = np.mean(img, axis=2) / 255.
-img = img[34:434, 4:-4]
+img = r"D:\anaconda\flappy_ai\FlapPyBird\datasets\start_night.jpg"
+img = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
+# img = np.mean(img, axis=2) / 255.
+img = img[34:434, 8:-8]
 [h, w] = img.shape
 
 
@@ -15,34 +15,67 @@ img = img[34:434, 4:-4]
 k = np.array([[-1, -1, -1],
                 [-1, 8, -1],
                 [-1, -1, -1]])
+# k = np.array([[0, -1, 0],
+#                 [-1, 4, -1],
+#                 [0, -1, 0]])
+
 
 s_k = np.ones([3, 3]) / 9
 img_edge = cv2.filter2D(cv2.filter2D(img, -1, k), -1, s_k)
 img_seg = img_edge != 0.0
 img_seg = img_seg.astype(np.float64)
-
+ls_k = np.ones([7, 7]) / 49
+img_seg = cv2.filter2D(img_seg, -1, ls_k)
+img_seg = img_seg > 0
+img_seg = img_seg.astype(np.float64)
 
 decision_map = np.zeros_like(img_edge)
 
 
 h_strip = img_seg[16, :]
-h_strip_shuffle  = np.pad(h_strip, pad_width=((0, 1)), mode='constant')
-h_sePoints = h_strip_shuffle[1:] - h_strip
-h_sePoints = np.argwhere(h_sePoints != 0).flatten()
-h_start_points = h_sePoints[::2]
-h_start_points -= 2
-h_end_points = h_sePoints[1::2]
-h_end_points += 2
-if len(h_end_points) - len(h_start_points) == 1:
-    end_points = h_end_points + [len(h_strip) - 1]
-h_ob_lr_tuple = np.array(list(zip(h_start_points, h_end_points)))
+# h_strip_shuffle  = np.pad(h_strip, pad_width=((0, 1)), mode='constant')
+h_sePoints = h_strip[1:] - h_strip[:-1]
+h_start_Points = np.argwhere(h_sePoints > 0).flatten() + 3
+h_end_Points = np.argwhere(h_sePoints < 0).flatten() - 3
 
-v_strip = img_seg[:, np.int((h_ob_lr_tuple[:, 0] + h_ob_lr_tuple[:, 1]) / 2)]
-v_strip_shuffle = np.pad(v_strip, pad_width=((1, 0)), mode='constant')
-v_sePoints = v_strip_shuffle[:-1] - v_strip
-v_sepoints = np.argwhere(v_sePoints != 0)
+if h_end_Points[0] < h_start_Points[0]:
+    h_end_Points = h_end_Points[1:]
+h_ob_lr_tuple = np.array(list(zip(h_start_Points, h_end_Points)))
+
+num_ob = h_ob_lr_tuple.shape[0]
+
+for i in range(num_ob):
+    s, e = h_ob_lr_tuple[i, :]
+    decision_map[:, s:e] = 1
+
+
+
+v_strip = img_seg[:, np.int32((h_ob_lr_tuple[:, 0] + h_ob_lr_tuple[:, 1]) / 2)]
+# v_strip_shuffle = np.pad(v_strip, pad_width=((1, 0)), mode='constant')
+v_sePoints = v_strip[1:, :] - v_strip[:-1, :]
+v_upper_Points = np.argwhere(v_sePoints < 0)
+v_upper_Points[:, 0] -= 3
+v_bot_Points = np.argwhere(v_sePoints > 0)
+v_bot_Points[:, 0] += 3
+
+v_sps_tb_tuple  = np.zeros([num_ob, 2], dtype=np.int32)
+
+for i in range(num_ob):
+    up_idx = np.argwhere(v_upper_Points == i)[:, 0]
+    ob_v_upper_group = v_upper_Points[up_idx, 0]
+    ob_v_bot_group = v_bot_Points[up_idx, 0]
+    current_space = np.array([ob_v_upper_group.min(), ob_v_bot_group.max()])
+    v_sps_tb_tuple[i, :] = np.int32(current_space)
+
+for i in range(num_ob):
+    l, r = h_ob_lr_tuple[i, :]
+    t, b = v_sps_tb_tuple[i, :]
+    decision_map[t:b, l:r] = 0
+
+
 
 print()
+
 
 
 
--- test_grapImg.py
+++ test_grapImg.py
@@ -2,6 +2,7 @@ import numpy as np
 from PIL import Image
 from PIL import ImageGrab
 import win32gui, win32con
+import time
 
 
 handle = win32gui.FindWindow(0, 'Flappy Bird')
@@ -9,5 +10,8 @@ bbox = win32gui.GetWindowRect(handle)
 text = win32gui.SetForegroundWindow(handle)
 win32gui.SendMessage(handle, win32con.WM_SYSCOMMAND, win32con.SC_RESTORE, 0)
 
-img = ImageGrab.grab(bbox=bbox)
-img.save('./test.png')
+time.sleep(3)
+
+for i in range(10):
+    img = ImageGrab.grab(bbox=bbox)
+    img.save('./test_{}.png'.format(i))
